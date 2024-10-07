3dd436e20ea502fa0f97eedea4f22a20e2b1d6d7
Chanhyukk <genichan0321@naver.com>
2024-10-07 11:05:09 +0900

    first commit

--- src/Week1/hw1.java
+++ src/Week1/hw1.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class hw1 {
     public static void main(String[] args) {
-        int v, r;
+        double v, r;
         Scanner sc = new Scanner(System.in);
         System.out.print("전압을 입력하세요: ");
-        v = sc.nextInt();
+        v = sc.nextDouble();
         System.out.print("저항을 입력하세요: ");
-        r = sc.nextInt();
-        int w = v * v / r;
+        r = sc.nextDouble();
+        double w = v * v / r;
         double dbm = 10 * Math.log10(w) + 30;
         System.out.printf("데시벨 밀리와트: %.1f\n", dbm);
     }
