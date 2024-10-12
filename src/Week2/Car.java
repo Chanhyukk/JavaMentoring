aac4cbd84f839b274c0002164bf5df46e683e719
Chanhyukk <genichan0321@naver.com>
2024-10-12 12:15:06 +0900

    first commit

--- src/Week2/Car.java
+++ src/Week2/Car.java
@@ -3,32 +3,32 @@ package Week2;
 public class Car {
     private int gas;
     private String carName;
-    public Car () {
+    Car () {
 
     }
-    public Car (String carName) {
+    Car (String carName) {
         setCarName(carName);
     }
-    public Car (int gas) {
+    Car (int gas) {
         setGas(gas);
     }
-    public Car (String carName, int gas) {
+    Car (String carName, int gas) {
         setCarName(carName);
         setGas(gas);
     }
-    void setGas (int gas) {
+    public void setGas (int gas) {
         this.gas = gas;
     }
-    int getGas () {
+    public int getGas () {
         return gas;
     }
-    void setCarName (String carName) {
+    public void setCarName (String carName) {
         this.carName = carName;
     }
-    String getCarName () {
+    public String getCarName () {
         return carName;
     }
-    int move (int gas) {
+    public int move (int gas) {
         if (this.gas >= gas) {
             setGas(this.gas - gas);
             System.out.println(getCarName() + " move, remain gas: " + getGas());
@@ -40,7 +40,7 @@ public class Car {
         }
 
     }
-    int refuel (int amount) {
+    public int refuel (int amount) {
         setGas(gas + amount);
         System.out.println("fuel amount: " + amount + ", resume fuel: " + getGas());
         return amount;
