13865e98cbba3c8219518a975936b7124f05dc68
Chanhyukk <genichan0321@naver.com>
2024-10-14 20:03:29 +0900

    first commit

--- src/Week3/Computer.java
+++ src/Week3/Computer.java
@@ -1,8 +1,11 @@
 package Week3;
 
 public class Computer {
-    private final String cpu = "CPU";
+    private final String cpu;
     private boolean power;
+    public Computer(String cpu) {
+        this.cpu = cpu;
+    }
     public boolean powerButton() {
         power = true;
         return power;
--- src/Week3/_3_1.java
+++ src/Week3/_3_1.java
@@ -2,7 +2,7 @@ package Week3;
 
 public class _3_1 {
     public static void main(String[] args) {
-        Computer computer = new Computer();
+        Computer computer = new Computer("m1");
         Monitor monitor = new Monitor(27);
         PersonalComputer pc = new PersonalComputer(computer, monitor);
         pc.turnOn();
